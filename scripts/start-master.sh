edb9971efa761a6c2087b914d0d4ae94eb9667b2
Arkadiusz Komarzewski <akkomar@users.noreply.github.com>
2016-02-16 13:37:52 +0100

    simplify startup scripts

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -2,4 +2,9 @@
 
 export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 
-/usr/local/spark/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+/usr/local/spark/bin/spark-class org.apache.spark.deploy.master.Master \
+  --properties-file /spark-defaults.conf \
+  --ip $SPARK_MASTER_IP \
+  --port $SPARK_MASTER_PORT \
+  --webui-port $SPARK_MASTER_WEBUI_PORT \
+	"$@"
--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd /usr/local/spark
+
 export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-./bin/spark-class org.apache.spark.deploy.worker.Worker \
+
+/usr/local/spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
 	spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} \
 	--properties-file /spark-defaults.conf \
-	-i $SPARK_LOCAL_IP \
 	"$@"
